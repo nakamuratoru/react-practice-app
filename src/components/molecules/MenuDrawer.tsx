c214836da0df8129f75679daa0cdf05be40afa6d
nakamuratoru <toru.n.2567@gmail.com>
2024-08-25 15:54:19 +0900

    72 ヘッダーのルーティング実装

--- src/components/molecules/MenuDrawer.tsx
+++ src/components/molecules/MenuDrawer.tsx
@@ -4,18 +4,21 @@ import { Button, Drawer, DrawerBody, DrawerContent, DrawerOverlay } from '@chakr
 type Props = {
     onClose: () => void;
     isOpen: boolean;
+    onClickHome: () => void;
+    onClickUserManagement: () => void;
+    onClickSetting: () => void;
 };
 
 export const MenuDrawer: VFC<Props> = memo((props) => {
-    const { onClose, isOpen } = props;
+    const { onClose, isOpen, onClickHome, onClickUserManagement, onClickSetting } = props;
     return (
         <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen} >
             <DrawerOverlay>
                 <DrawerContent>
                     <DrawerBody p={0} bg="gray.100">
-                        <Button w="100%">TOP</Button>
-                        <Button w="100%">UserList</Button>
-                        <Button w="100%">Setting</Button>
+                        <Button w="100%" onClick={onClickHome}>TOP</Button>
+                        <Button w="100%" onClick={onClickUserManagement}>UserList</Button>
+                        <Button w="100%" onClick={onClickSetting}>Setting</Button>
                     </DrawerBody>
                 </DrawerContent>
             </DrawerOverlay>
--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,11 +1,17 @@
-import { VFC, memo } from "react";
+import { VFC, memo, useCallback } from "react";
 import { Box, Flex, Heading, Link, useDisclosure} from "@chakra-ui/react";
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 import { MenuDrawer } from "../../molecules/MenuDrawer";
+import { useHistory } from "react-router";
 
 export const Header: VFC = memo(() => {
     
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const history = useHistory();
+
+    const onClickHome = useCallback(() => history.push("/home"), []);
+    const onClickUserManagement = useCallback(() => history.push("/home/user_management"), []);
+    const onClickSetting = useCallback(() => history.push("/home/setting"), []);
 
     return (
         <>
@@ -15,19 +21,28 @@ export const Header: VFC = memo(() => {
             color="gray.50" 
             align="center" 
             justify="space-between" 
-            padding={{ base: "md", md: "lg" }}
+            padding={{ base: 3, md: 5 }}
         >
-            <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
-                UserManagementApp
-            </Heading> 
-            <Flex align="center" as="a" mr={8} fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
+            <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer"}} onClick={onClickHome}>
+                <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+                    UserManagementApp
+                </Heading> 
+            </Flex>
+            
+            <Flex align="center" mr={8} fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
                 <Box pr={4}>
-                <Link>UserList</Link>
+                <Link onClick={onClickUserManagement}>UserList</Link>
                 </Box>
+                <Link onClick={onClickSetting}>Setting</Link>
             </Flex>
             <MenuIconButton onOpen={onOpen} />
         </Flex>
-        <MenuDrawer onClose={onClose} isOpen={isOpen}/>
+        <MenuDrawer 
+            onClose={onClose} 
+            isOpen={isOpen} 
+            onClickHome={onClickHome} 
+            onClickUserManagement={onClickUserManagement} 
+            onClickSetting={onClickSetting}/>
         </>
     );
 });
