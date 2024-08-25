6e4e3ac01cb35481d18e86902c9883b1119fd694
nakamuratoru <toru.n.2567@gmail.com>
2024-08-25 16:25:27 +0900

    74 ログイン画面のコンポーネント分割

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,17 +1,20 @@
 import { VFC, memo } from "react";
-import { Box, Divider, Flex, Heading, Input, Button, Stack} from "@chakra-ui/react";
+import { Box, Divider, Flex, Heading, Input, Stack} from "@chakra-ui/react";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: VFC = memo(() => {
     return (
         <Flex align="center" justify="center" height="100vh">
             <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
-                <Heading as="h1" size="lg" textAlign="center">UserManagementApp</Heading>
+                <Heading as="h1" size="lg" textAlign="center">
+                    UserManagementApp
+                </Heading>
                 <Divider my={4} />
                 <Stack spacing={6} py={4} px={10}>
-                <Input placeholder="userId" />
-                <Button bg="teal.400" color="white" _hover={{ opacity: 0.8}}>Login</Button>
+                    <Input placeholder="userId" />
+                    <PrimaryButton>Login</PrimaryButton>
                 </Stack>
             </Box>
-            </Flex>
+        </Flex>
     );
-});
+})
