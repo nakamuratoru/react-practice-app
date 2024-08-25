a7aa8415d7a4c030185bab52220c4f668598e12c
nakamuratoru <toru.n.2567@gmail.com>
2024-08-25 17:15:57 +0900

    76 メッセージ表示機能の実装

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,22 +2,27 @@ import axios from "axios"
 import { User } from "../types/api/user";
 import { useCallback, useState } from "react";
 import { useHistory } from "react-router";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
     const history = useHistory();
 
     const [loading, setLoading] = useState(false);
 
+    const { showMessage } = useMessage();
+
     const login = useCallback((id: string) => {
         setLoading(true);
         axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
             if (res.data) {
+                showMessage({ title: "login", status: "success"});
                 history.push("/home");
             } else {
-                alert("not found user");
+                showMessage({ title: "not found user", status: "error"});
             }
         })
-        .catch(() => alert("failed to login"))
+        .catch(() => 
+        showMessage({ title: "failed to login", status: "error"}))
         .finally(() => setLoading(false));
     }, [history]);
     return { login, loading };
